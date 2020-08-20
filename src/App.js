c2bb8739adaecc5afb495c34b151db56e3e582ec
Prechar <pxiong037@gmail.com>
2020-08-20 09:22:58 +0000

    Add Drawer to NewPaletteForm

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
 import PaletteList from './PaletteList';
 import SingleColorPalette from './SingleColorPalette';
+import NewPaletteForm from './NewPaletteForm';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 
@@ -16,6 +17,10 @@ function findPalette(id){
 function App() {
   return (
 	<Switch>
+		<Route 
+			exact path='/palette/new'	
+			render={() => <NewPaletteForm/>}
+		/>
 		<Route 
 			exact path='/' 
 			render={(routeProps) => 
