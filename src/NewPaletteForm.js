6e513b1c3fac45a442bdd6fb3680347bb1a3efb7
Prechar <pxiong037@gmail.com>
2020-08-21 07:56:45 +0000

    Add ChromePicker and Buttons to drawer

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -10,8 +10,10 @@ import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import { ChromePicker } from 'react-color';
+import Button from '@material-ui/core/Button';
 
-const drawerWidth = 240;
+const drawerWidth = 400;
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -83,54 +85,60 @@ function NewPaletteForm(){
   };
 
   return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open,
-        })}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            className={clsx(classes.menuButton, open && classes.hide)}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap>
-            Persistent drawer
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        className={classes.drawer}
-        variant="persistent"
-        anchor="left"
-        open={open}
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-      >
-        <div className={classes.drawerHeader}>
-          <IconButton onClick={handleDrawerClose}>
-        	<ChevronLeftIcon />
-          </IconButton>
-        </div>
-        <Divider />
-      </Drawer>
-      <main
-        className={clsx(classes.content, {
-          [classes.contentShift]: open,
-        })}
-      >
-        <div className={classes.drawerHeader} />
-        
-      </main>
-    </div>
+	<div className={classes.root}>
+		<CssBaseline />
+		<AppBar
+		position="fixed"
+		className={clsx(classes.appBar, {
+		  [classes.appBarShift]: open,
+		})}
+		>
+		<Toolbar>
+			<IconButton
+				color="inherit"
+				aria-label="open drawer"
+				onClick={handleDrawerOpen}
+				edge="start"
+				className={clsx(classes.menuButton, open && classes.hide)}
+			>
+				<MenuIcon />
+		  	</IconButton>
+			<Typography variant="h6" noWrap>Persistent drawer</Typography>
+		</Toolbar>
+		</AppBar>
+		<Drawer
+			className={classes.drawer}
+			variant="persistent"
+			anchor="left"
+			open={open}
+			classes={{
+			  paper: classes.drawerPaper,
+			}}
+		>
+			<div className={classes.drawerHeader}>
+				<IconButton onClick={handleDrawerClose}>
+				<ChevronLeftIcon />
+				</IconButton>
+			</div>
+			<Divider />
+			<Typography variant='h4'>Design Your Palette</Typography>
+			<div>
+				<Button variant='contained' color='secondary'>Clear Palette</Button>
+				<Button variant='contained' color='primary'>Random Color</Button>
+			</div>
+			<ChromePicker 
+				onChangeComplete={newColor => console.log(newColor)}
+			/>
+			<Button variant='contained' color='primary'>Add Color</Button>
+		</Drawer>
+		<main
+			className={clsx(classes.content, {
+			  [classes.contentShift]: open,
+			})}
+		>
+			<div className={classes.drawerHeader} />
+		</main>
+	</div>
   );
 }
 
