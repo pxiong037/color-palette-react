6482557e1b1fed61a7d64c3f2c9987a4dc33ed3e
Prechar <pxiong037@gmail.com>
2020-08-21 08:44:14 +0000

    Connect colorpicker to button

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
@@ -74,72 +74,95 @@ const useStyles = makeStyles((theme) => ({
 
 function NewPaletteForm(){
 	const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
+  	const [open, setOpen] = useState(false);
+  	const [currentColor, setColor] = useState('teal');
+  	const [colors, setColors] = useState(['purple','red']);
 
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
+  	const handleDrawerOpen = () => {
+    	setOpen(true);
+	};
 
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
+	const handleDrawerClose = () => {
+	setOpen(false);
+	};
 
-  return (
-	<div className={classes.root}>
-		<CssBaseline />
-		<AppBar
-		position="fixed"
-		className={clsx(classes.appBar, {
-		  [classes.appBarShift]: open,
-		})}
-		>
-		<Toolbar>
-			<IconButton
-				color="inherit"
-				aria-label="open drawer"
-				onClick={handleDrawerOpen}
-				edge="start"
-				className={clsx(classes.menuButton, open && classes.hide)}
-			>
-				<MenuIcon />
-		  	</IconButton>
-			<Typography variant="h6" noWrap>Persistent drawer</Typography>
-		</Toolbar>
-		</AppBar>
-		<Drawer
-			className={classes.drawer}
-			variant="persistent"
-			anchor="left"
-			open={open}
-			classes={{
-			  paper: classes.drawerPaper,
-			}}
-		>
-			<div className={classes.drawerHeader}>
-				<IconButton onClick={handleDrawerClose}>
-				<ChevronLeftIcon />
-				</IconButton>
-			</div>
-			<Divider />
-			<Typography variant='h4'>Design Your Palette</Typography>
-			<div>
-				<Button variant='contained' color='secondary'>Clear Palette</Button>
-				<Button variant='contained' color='primary'>Random Color</Button>
-			</div>
-			<ChromePicker 
-				onChangeComplete={newColor => console.log(newColor)}
-			/>
-			<Button variant='contained' color='primary'>Add Color</Button>
-		</Drawer>
-		<main
-			className={clsx(classes.content, {
-			  [classes.contentShift]: open,
+	const updateCurrentColor = (newColor) => {
+	  setColor(newColor.hex);
+	}
+	
+	const addNewColor = () => {
+		setColors([...colors, currentColor]);
+	}
+	
+  	return (
+		<div className={classes.root}>
+			<CssBaseline />
+			<AppBar
+			position="fixed"
+			className={clsx(classes.appBar, {
+			  [classes.appBarShift]: open,
 			})}
-		>
-			<div className={classes.drawerHeader} />
-		</main>
-	</div>
-  );
+			>
+			<Toolbar>
+				<IconButton
+					color="inherit"
+					aria-label="open drawer"
+					onClick={handleDrawerOpen}
+					edge="start"
+					className={clsx(classes.menuButton, open && classes.hide)}
+				>
+					<MenuIcon />
+				</IconButton>
+				<Typography variant="h6" noWrap>Persistent drawer</Typography>
+			</Toolbar>
+			</AppBar>
+			<Drawer
+				className={classes.drawer}
+				variant="persistent"
+				anchor="left"
+				open={open}
+				classes={{
+				  paper: classes.drawerPaper,
+				}}
+			>
+				<div className={classes.drawerHeader}>
+					<IconButton onClick={handleDrawerClose}>
+					<ChevronLeftIcon />
+					</IconButton>
+				</div>
+				<Divider />
+				<Typography variant='h4'>Design Your Palette</Typography>
+				<div>
+					<Button variant='contained' color='secondary'>Clear Palette</Button>
+					<Button variant='contained' color='primary'>Random Color</Button>
+				</div>
+				<ChromePicker 
+					color={currentColor}
+					onChange={updateCurrentColor}
+				/>
+				<Button 
+					variant='contained' 
+					color='primary'
+					style={{backgroundColor: currentColor}}
+					onClick={addNewColor}
+				>
+					Add Color
+				</Button>
+			</Drawer>
+			<main
+				className={clsx(classes.content, {
+				  [classes.contentShift]: open,
+				})}
+			>
+				<div className={classes.drawerHeader} />
+				<ul>
+					{colors.map(color => (
+						<li style={{backgroundColor: color}}>{color}</li>
+					))}
+				</ul>
+			</main>
+		</div>
+  	);
 }
 
 export default NewPaletteForm;
