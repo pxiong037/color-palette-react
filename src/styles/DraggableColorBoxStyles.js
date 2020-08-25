96fd470651d7f9c49e8cd8e86022387e91b5859b
Prechar <pxiong037@gmail.com>
2020-08-25 08:05:17 +0000

    Tweak styles of form

--- src/styles/DraggableColorBoxStyles.js
+++ src/styles/DraggableColorBoxStyles.js
@@ -6,7 +6,7 @@ const styles = {
 		display: 'inline-block',
 		position: 'relative',
 		cursor: 'pointer',
-		marginBottom: '-3.5px',
+		marginBottom: '-5.77px',
 		'&:hover svg':{
 			color: 'white',
 			transform: 'scale(1.5)'
