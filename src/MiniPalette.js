0e93edb1e1fa267462d64647b70c60e9ca67e661
Prechar <pxiong037@gmail.com>
2020-08-25 08:42:10 +0000

    Add deleteIcon to MiniPalette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,49 +1,8 @@
 import React from 'react';
 import { withStyles } from '@material-ui/styles';
 import { Twemoji } from 'react-emoji-render';
-
-const styles = {
-	root: {
-		backgroundColor: 'white',
-		border: '1px solid black',
-		borderRadius: '5px',
-		padding: '0.5rem',
-		position: 'relative',
-		overflow: 'hidden',
-		'&:hover': {
-			cursor: 'pointer'
-		}
-	},
-	colors: {
-		backgroundColor: '#dae1e4',
-		height: '150px',
-		width: '100%',
-		borderRadius: '5px',
-		overflow: 'hidden'
-	},
-	title: {
-		display: 'flex',
-		justifyContent: 'space-between',
-		alignItems: 'center',
-		margin: '0',
-		color: 'black',
-		paddingTop: '0.5rem',
-		fontSize: '1rem',
-		position: 'relative'
-	},
-	emoji: {
-		marginLeft: '0.5rem',
-		fontSize: '1.5rem'
-	},
-	miniColor: {
-		height: '25%',
-		width: '20%',
-		display: 'inline-block',
-		margin: '0 auto',
-		marginBottom: '-3.5px'
-	},
-	
-}
+import DeleteIcon from '@material-ui/icons/Delete';
+import styles from './styles/MiniPaletteStyles';
 
 function MiniPalette(props){
 	const { classes, paletteName, emoji, colors, handleClick } = props;
@@ -56,8 +15,11 @@ function MiniPalette(props){
 	))
 	return(
 		<div className={classes.root} onClick={handleClick}>
+			<div className={classes.delete}>
+				<DeleteIcon className={classes.deleteIcon} style={{transition: 'all 0.3s ease-in-out'}}/>
+			</div>
 			<div className={classes.colors}>
-			{miniColorBoxes}
+				{miniColorBoxes}
 			</div>
 			<h5 className={classes.title}>{paletteName} <Twemoji className={classes.emoji} text={`${emoji}`} /></h5>
 		</div>
