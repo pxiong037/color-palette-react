03324ffbd7344b1e5cb08aeac82b806cd2f841f4
Prechar <pxiong037@gmail.com>
2020-08-26 10:40:38 +0000

    Updates

---
+++ precache-manifest.62820ad94a412330901470037212f21d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03eb6ab49be26ccb19b233dd28bb8717",
+    "url": "/color-palette-react/index.html"
+  },
+  {
+    "revision": "00288e52acacb42e7e78",
+    "url": "/color-palette-react/static/css/2.2a4460b4.chunk.css"
+  },
+  {
+    "revision": "4b9610a3a6ffdd32199d",
+    "url": "/color-palette-react/static/css/main.84d63943.chunk.css"
+  },
+  {
+    "revision": "00288e52acacb42e7e78",
+    "url": "/color-palette-react/static/js/2.d727a9bc.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/color-palette-react/static/js/2.d727a9bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b9610a3a6ffdd32199d",
+    "url": "/color-palette-react/static/js/main.1dbf4ef7.chunk.js"
+  },
+  {
+    "revision": "0dd38bb6135de1ae02bc",
+    "url": "/color-palette-react/static/js/runtime-main.640afa7b.js"
+  },
+  {
+    "revision": "f264b11539637d66d295e69b9c2414b3",
+    "url": "/color-palette-react/static/media/bg.f264b115.svg"
+  }
+]);
